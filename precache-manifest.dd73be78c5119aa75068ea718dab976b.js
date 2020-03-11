5867681adb3b0b9e4c16f144eb86a8b7de3f7e8b
Sandino <paulosandino@hotmail.com>
2020-03-10 23:45:07 -0300

    Updates

---
+++ precache-manifest.dd73be78c5119aa75068ea718dab976b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d70114f5e21c0b05c0c25f58f13d9b7",
+    "url": "/react-pipefy-clone/index.html"
+  },
+  {
+    "revision": "995a4623c7c2348fc719",
+    "url": "/react-pipefy-clone/static/js/2.7c2ecd0d.chunk.js"
+  },
+  {
+    "revision": "2d4e50db9bebc1822939159016933eb5",
+    "url": "/react-pipefy-clone/static/js/2.7c2ecd0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15abdfd3c7f557b40bd6",
+    "url": "/react-pipefy-clone/static/js/main.bc4c9be0.chunk.js"
+  },
+  {
+    "revision": "49310e9b4212a3f957eb",
+    "url": "/react-pipefy-clone/static/js/runtime-main.fbf7a742.js"
+  }
+]);
